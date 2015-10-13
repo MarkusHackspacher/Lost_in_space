4fa66e82956a3421838b99eceef05283e4e0ce4a
MarkusHackspacher <hackspacher@gmx.de>
2015-10-13 18:11:04 +0200

    change

--- test/test_pep8.py
+++ test/test_pep8.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 
-# pyLottoSimu
+# lost_in_space
 
 # Copyright (C) <2015> Markus Hackspacher
 
-# This file is part of pyLottoSimu.
+# This file is part of lost_in_space.
 
-# pyLottoSimu is free software: you can redistribute it and/or modify
+# lost_in_space is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# pyLottoSimu is distributed in the hope that it will be useful,
+# lost_in_space is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with pyLottoSimu.  If not, see <http://www.gnu.org/licenses/>.
+# along with lost_in_space.  If not, see <http://www.gnu.org/licenses/>.
 
 import unittest
 import pep8
